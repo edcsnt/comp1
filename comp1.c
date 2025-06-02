93c29f702cfac1a73d2c0427983a432f5549ac6e
Eduardo Santos <edsantos@redhat.com>
2025-06-02 19:07:17 -0300

    feat: handle errors

--- comp1.c
+++ comp1.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int
-main()
+main(int argc, char *argv[])
 {
-	printf("Hello, world!\n");
+	if (argc > 1) {
+		fprintf(stderr,
+		        "\e[1;91merr: comp1 takes no arguments\e[0m\n");
+		return 1;
+	}
+	fprintf(stderr, "\e[1;95mHello, world!\e[0m\n");
 	return 0;
 }
