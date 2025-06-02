abf5ad765c42c60280051f5d408187e134a52a08
Eduardo Santos <edsantos@redhat.com>
2025-06-02 19:14:37 -0300

    chore: put reused ANSI color code into variable

--- comp1.c
+++ comp1.c
@@ -3,9 +3,11 @@
 int
 main(int argc, char *argv[])
 {
+	char reset[7] = "\e[1;91m";
 	if (argc > 1) {
 		fprintf(stderr,
-		        "\e[1;91merr: comp1 takes no arguments\e[0m\n");
+		        "%serr: comp1 takes no arguments\e[0m\n",
+		        reset);
 		return 1;
 	}
 	fprintf(stderr, "\e[1;95mHello, world!\e[0m\n");
